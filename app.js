b6f50d5553a45a75c80f806d7aea057f9aa3a6b5
Joseph <92699311+JMCampbell2021@users.noreply.github.com>
2022-01-19 22:38:45 -0500

    updated app.js

--- app.js
+++ app.js
@@ -1,4 +1,7 @@
-const inquirer = require('inquirer')
+const fs = require('fs');
+const inquirer = require('inquirer');
+const generatePage = require('./src/page-template');
+
 const promptUser = () => {
   return inquirer.prompt([
     {
@@ -50,15 +53,15 @@ const promptUser = () => {
 };
 
 const promptProject = portfolioData => {
-  // If there's no 'projects' array property, create one
-  if (!portfolioData.projects) {
-    portfolioData.projects = [];
-  }
   console.log(`
   =================
   Add a New Project
   =================
   `);
+   // If there's no 'projects' array property, create one
+   if (!portfolioData.projects) {
+    portfolioData.projects = [];
+  }
   return inquirer.prompt([
     {
       type: 'input',
@@ -133,13 +136,8 @@ promptUser()
   .then(portfolioData => {
     console.log(portfolioData);
   });
-// const fs = require('fs');
-// const generatePage = require('./src/page-template');
-
 // const pageHTML = generatePage(name, github);
-
 // fs.writeFile('./index.html', pageHTML, err => {
 //   if (err) throw err;
-
 //   console.log('Portfolio complete! Check out index.html to see the output!');
 // });
